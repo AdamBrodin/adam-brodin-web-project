ba6c525e00f2ea090a73cf16c09002b365f7eb2c
Adam Brodin <worecraft@gmail.com>
2021-02-23 22:18:00 +0100

    Merged login system from Okta to Firebase

---
+++ js/login.js
@@ -0,0 +1,14 @@
+var uiConfig = {
+    signInSuccessUrl: 'account.html',
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    ],
+
+    tosUrl: 'index.html'
+};
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', uiConfig);
---
+++ js/main.js
@@ -0,0 +1,34 @@
+var mainApp = {};
+(function () {
+    var mainContainer = document.getElementById("main_container");
+    var authContainer = document.getElementById("firebaseui-auth-container");
+    var welcomeUserText = document.getElementById("welcome-user-text");
+
+    var logout = function () {
+        firebase.auth().signOut().then(function () {
+            window.location.replace("index.html");
+        }, function () { })
+    }
+
+    var init = function () {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                mainContainer.style.display = "";
+
+                // Hides the login form
+                authContainer.style.display = "none";
+
+                // Changes the text to welcome the user;
+                welcomeUserText.innerHTML = "Welcome " + firebase.auth().currentUser.displayName + "!";
+            } else {
+                // No user is signed in.
+                mainContainer.style.display = "none";
+            }
+        });
+    }
+
+    init();
+
+    mainApp.logout = logout;
+})();
